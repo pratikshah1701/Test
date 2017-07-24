e824ec074637d5e372a3e5746168c317a8afb294
pshah <pshah@internal.lowermybills.com>
2017-07-24 14:10:05 -0700

    merge test 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,14 @@ package test;
 
 public class HelloWorld {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalArgumentException {
+		try{
 		System.out.println("hello world");
+		}catch(Exception e){
+			System.out.println("exception caught");
+		}
 	}
 
 }
+
+
